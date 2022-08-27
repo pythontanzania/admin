e49f316ec683b5bf38f3f1b970e0f6bd556c9afe
Jamal <87450618+AvicennaJr@users.noreply.github.com>
2022-08-27 04:02:57 +0300

    Update models.py

--- speakers/models.py
+++ speakers/models.py
@@ -3,10 +3,12 @@ from phone_field import PhoneField
 
 # Create your models here.
 
+class Speaker(models.Model):
+    speaker_name = models.CharField(max_length=200)
+    speaker_field = models.CharField(max_length=500)  # speaker career/expertise
+    speaker_topic = models.CharField(max_length=500)
+    speaker_email = models.EmailField()
+    speaker_phone = PhoneField(blank=True, help_text="Contact phone number")
 
-class Speakers(models.Model):
-    name = models.CharField(max_length=200)
-    field = models.CharField(max_length=500)
-    topic = models.CharField(max_length=500)
-    email = models.EmailField()
-    phone = PhoneField(blank=True, help_text="Contact phone number")
+    def __str__(self):
+        return self.speaker_name
