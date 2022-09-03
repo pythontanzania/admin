5c72b0ee2f534ee331662dde6383f8d52dd56ff3
mtuchi <mtuchidev@gmail.com>
2022-09-03 16:03:26 +0300

    fix: linting errors

--- eventTimeTable/views.py
+++ eventTimeTable/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from .models import Event
 from .serializers import EventSerializer
 
+
 # Everybody can access
 class EventsList(generics.ListAPIView):
     queryset = Event.objects.all()
@@ -13,12 +14,12 @@ class EventsList(generics.ListAPIView):
 
 # Only Admin can access
 class EventCreate(generics.CreateAPIView):
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (permissions.IsAdminUser, )
     serializer_class = EventSerializer
 
 
 # Only Admin can access
 class EventsDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (permissions.IsAdminUser, )
     queryset = Event.objects.all()
     serializer_class = EventSerializer
--- speakers/views.py
+++ speakers/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from .models import Speaker
 from .serializers import SpeakerSerializer, SpeakerCreateSerializer
 
+
 # Everyone should access
 class SpeakersList(generics.ListAPIView):
     queryset = Speaker.objects.all()
@@ -18,6 +19,6 @@ class SpeakersCreate(generics.CreateAPIView):
 
 # Only Admin can edit and delete
 class SpeakersDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (permissions.IsAdminUser, )
     queryset = Speaker.objects.all()
     serializer_class = SpeakerSerializer
